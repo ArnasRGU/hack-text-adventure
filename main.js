26ccf9c4af6602b67879f3d42353ebe62d9327fe
Arnas Seremeta <1908019@rgu.ac.uk>
2020-02-22 12:50:58 +0000

    added index.html

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 rl = require("readline-sync");
 fs = require("fs");
+sleep = require("sleep")
 
-cheems = fs.readFileSync("imgs/cheems.txt");
-console.log(cheems.toString())
+
+cheems = fs.readFileSync("imgs/cheems.txt").toString();
+
+function printSlowly(str,delay,i = 0) {
+	if (i === str.length-1) {
+		process.stdout.write(str[i])
+	} else {
+		process.stdout.write(str[i])
+		setTimeout(printSlowly,delay,str,delay,i+1)
+	}
+}
+
+printSlowly("hello",100);
+printSlowly("hello",100);
+printSlowly("hello",100);
+printSlowly("hello",100);
 
 
 
