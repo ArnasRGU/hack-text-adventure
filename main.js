15d787792acaa6bcdb1f4adc6bd72f5b584aae38
0John0 <56393501+0John0@users.noreply.github.com>
2020-02-23 01:37:08 +0000

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function beefJerkyBreakfast() {
 	writeDialog('<b>"Its really chewy... mother must have overcooked them again..."</b>');
 	writeDialog('<b><span style="color:#b0378a">You had better be leav-</span></b>');
 	changeImg2("images/cheemsMotherCry.png");
-	writeDialog("<b>It's no wonder Dad left us, you can't even cook scrambled eggs properly!</b>);
+	writeDialog("<b>It's no wonder Dad left us, you can't even cook scrambled eggs properly!</b>");
 	getChoices(["<i>leave the house</i>"],[frontYardBad]);
 }
 
