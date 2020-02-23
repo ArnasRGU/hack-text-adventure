d8d5a42d5609f1f1e89a464cbb781ad0e389b102
0John0 <56393501+0John0@users.noreply.github.com>
2020-02-23 02:57:24 +0000

    Update main.js

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ function busLate() {
 	name2.innerHTML = "";
 	info2.innerHTML = "";
 	changeBackground("images/houseExterior.jpg");
-	writeDialog("<b>Due to your argument with Mother, you have missed the bus.<br>You may be able to catch it at before it reaches it's next stop.</b>")
+	writeDialog("<b>Due to your argument with Mother, you have missed the bus.<br>You may be able to catch it before it reaches it's next stop.</b>")
 	getChoices(["<i>run to the next bus stop</i>","return home"],[street1,kitchen2]);
 }
 
